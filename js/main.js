0dab1aad273348e547b7544cba9fce951fbeef97
Turjo Khan <hturjokhan@gmail.com>
2021-07-04 23:48:14 +0600

    just apply js for structure list section

---
+++ js/main.js
@@ -0,0 +1,75 @@
+// Start ready function//
+$(document).ready(function(){
+
+    $(".l1").click(function(){
+       $(".l1").addClass("active");
+       $(".l2").removeClass("active");
+       $(".l3").removeClass("active");
+       $(".l4").removeClass("active");
+       $(".l5").removeClass("active");
+       $(".l6").removeClass("active");
+       $(".l7").removeClass("active");
+    });
+
+    $(".l2").click(function(){
+        $(".l1").removeClass("active");
+        $(".l2").addClass("active");
+        $(".l3").removeClass("active");
+        $(".l4").removeClass("active");
+        $(".l5").removeClass("active");
+        $(".l6").removeClass("active");
+        $(".l7").removeClass("active");
+     });
+     
+    $(".l3").click(function(){
+        $(".l1").removeClass("active");
+        $(".l2").removeClass("active");
+        $(".l3").addClass("active");
+        $(".l4").removeClass("active");
+        $(".l5").removeClass("active");
+        $(".l6").removeClass("active");
+        $(".l7").removeClass("active");
+     });
+
+     $(".l4").click(function(){
+        $(".l1").removeClass("active");
+        $(".l2").removeClass("active");
+        $(".l3").removeClass("active");
+        $(".l4").addClass("active");
+        $(".l5").removeClass("active");
+        $(".l6").removeClass("active");
+        $(".l7").removeClass("active");
+     });
+
+     $(".l5").click(function(){
+        $(".l1").removeClass("active");
+        $(".l2").removeClass("active");
+        $(".l3").removeClass("active");
+        $(".l4").removeClass("active");
+        $(".l5").addClass("active");
+        $(".l6").removeClass("active");
+        $(".l7").removeClass("active");
+     });
+
+     $(".l6").click(function(){
+        $(".l1").removeClass("active");
+        $(".l2").removeClass("active");
+        $(".l3").removeClass("active");
+        $(".l4").removeClass("active");
+        $(".l5").removeClass("active");
+        $(".l6").addClass("active");
+        $(".l7").removeClass("active");
+     });
+
+     $(".l7").click(function(){
+        $(".l1").removeClass("active");
+        $(".l2").removeClass("active");
+        $(".l3").removeClass("active");
+        $(".l4").removeClass("active");
+        $(".l5").removeClass("active");
+        $(".l6").removeClass("active");
+        $(".l7").addClass("active");
+     }); 
+
+});
+// end ready function //
